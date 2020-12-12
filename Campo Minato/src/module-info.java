c76bd9183e984e0adceb09acf5475b78bc602748
Tommaso Sgroi <tommaso_sgroi@yahoo.it>
2020-12-12 15:21:44 +0100

    Initialized basic gui

---
+++ Campo Minato/src/module-info.java
@@ -0,0 +1,11 @@
+module Campo.Minato {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.swing;
+    requires javafx.media;
+    requires javafx.web;
+    requires javafx.swt;
+    opens tommy.apllication.campo_minato;
+}
